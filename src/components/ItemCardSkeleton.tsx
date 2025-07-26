09d86de7dccb27db3f15812d282aa58eebd93c53
Saswata Das <dassaswwata1996@gmail.com>
2025-07-26 13:15:16 +0530

    add load skeleton

--- src/components/ItemCardSkeleton.tsx
+++ src/components/ItemCardSkeleton.tsx
@@ -1,5 +1,13 @@
-function ItemCardSkeleton() {
-    return (<>Item Card Skeleton</>)
+import { Box, Skeleton } from "@mui/material";
+import "../styles/ItemCard.styles.css"
+
+function ItemCard() {
+    return (
+        <Box className="itemCardContainer itemColumnFlex itemCardSkeletonContainer">
+            <Skeleton height={'90%'} width={'100%'} />
+            <Skeleton height={30} width={'100%'} />
+        </Box>
+    )
 }
 
-export default ItemCardSkeleton;
+export default ItemCard;
