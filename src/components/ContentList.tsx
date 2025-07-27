a5c6b3f791cc06fe01669ccac72323e3cac68bf4
Saswata Das <dassaswwata1996@gmail.com>
2025-07-27 14:00:32 +0530

    update loading mechanism

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -1,22 +1,76 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import type { ContentListProps } from "../types";
 import ItemCard from "./ItemCard";
 import ItemCardSkeleton from "./ItemCardSkeleton";
 import "../styles/ContentList.styles.css";
+import { useEffect, useRef, useState } from "react";
+import { itemLoadLimit } from "../constants";
 
 const skeletonArray = Array.from({length: 12}, (_,i)=>i);
 
 function ContentList(props:ContentListProps) {
     const { loading, showList = [] } = props;
+    const paginatorRef = useRef(null);
+    const [paginate, setPaginate] = useState(false);
+    const [itemLimit, setItemLimit] = useState(itemLoadLimit);
+
+    useEffect(() => {
+        if (!loading) {
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    setPaginate(entry.isIntersecting);
+                },
+                {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: 0.1
+                }
+            );
+
+            const currentPaginator = paginatorRef.current;
+            if(currentPaginator) {
+                observer.observe(currentPaginator);
+            }
+
+            return () => {
+                if (currentPaginator) {
+                    observer.unobserve(currentPaginator);
+                }
+            }
+        }
+    }, [loading])
+
+    const handleLoadMore = () => {
+        if (showList.length - itemLimit >= itemLoadLimit) {
+            setItemLimit(itemLimit + itemLoadLimit);
+        } else {
+            setItemLimit(itemLimit + (showList.length - itemLimit));
+        }
+    }
+
+    useEffect(() => {
+        if (paginate) {
+            handleLoadMore()
+        }
+    }, [paginate])
+
+
+    if (loading) {
+        return (
+            <Box className="contentListContainer">
+                {skeletonArray.map((i) => <ItemCardSkeleton key={i} />)}
+            </Box>
+        )
+    }
 
     return(
         <Box className="contentListContainer">
             {
-                loading && skeletonArray.map((i) => <ItemCardSkeleton key={i} />)
+                !loading && showList.map((item, index) => index < itemLimit && <ItemCard key={item.id} {...item} />)
             }
-            {
-                !loading && showList.map((item) => <ItemCard key={item.id} {...item} />)
-            }
+            <div style={{ display: itemLimit < showList.length ? 'block' : 'none' }} ref={paginatorRef} >
+                <Button onClick={handleLoadMore} variant="text">Load more</Button>
+            </div>
         </Box>
     )
 }
